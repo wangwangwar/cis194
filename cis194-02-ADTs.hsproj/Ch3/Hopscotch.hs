537825669d655c329aea824536af3821e8daf252
wangwangwar <wangwangwar@gmail.com>
2016-12-08 11:25:47 +0800

    Add 03-rec-poly Homework Exercise 1 Hopscotch

--- cis194-02-ADTs.hsproj/Ch3/Hopscotch.hs
+++ cis194-02-ADTs.hsproj/Ch3/Hopscotch.hs
@@ -1,17 +1,30 @@
 module Ch3.Hopscotch where
 
-skipsInner :: Int -> [a] -> ([a], [a])
-skipsInner _ [] = ([], [])
-skipsInner 0 list = ([], list)
-skipsInner 1 [x:xs] = ([x], xs)
-skipsInner n [x:xs] = skipsInner (n - 1) xs
+-- Get a tuple of n-th element and the rest list after the n-th element.
+skipsNth :: Int -> [a] -> ([a], [a])
+skipsNth _ [] = ([], [])
+skipsNth 0 list = ([], list)
+skipsNth 1 (x:xs) = ([x], xs)
+skipsNth n (x:xs) = skipsNth (n - 1) xs
 
-skipsInner2 :: Int -> [a] -> [a]
-skipsInner2 n list 
-    let 
-        (result, tail) = skipsInner n list
-    in
-        | tail == [] = result
-        | otherwise = result + skipsInner n tail
+-- Skips every n-th elements.
+skipsEveryNth :: Int -> [a] -> [a]
+skipsEveryNth n list 
+    | n == 0 = list
+    | null tail = result
+    | otherwise = result ++ skipsEveryNth n tail
+    where
+        (result, tail) = skipsNth n list
   
---skips :: [a] -> [[a]]
+-- The output of skips is a list of lists. The first list in the output
+-- should be the same as the input list. The second list in the output
+-- should contain every second element from the input list...and the nth
+-- list in the output should contain every nth element from the input list.
+skips :: [a] -> [[a]]
+skips list = 
+    skipsInner 1 list []
+    where 
+        skipsInner n list acc
+            | n <= 0 = []
+            | n > length list = acc
+            | otherwise = skipsInner (n + 1) list (acc ++ [(skipsEveryNth n list)])
