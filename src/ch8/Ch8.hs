1eccd99efffedf9c60404168da491d849e642a1f
wangwangwar <wangwangwar@gmail.com>
2016-12-22 20:25:42 +0800

    Ch8 Exercise 1

---
+++ src/ch8/Ch8.hs
@@ -0,0 +1,43 @@
+
+-- http://www.seas.upenn.edu/~cis194/spring13/hw/08-IO.pdf
+
+module Ch8
+    (
+    glCons,
+    moreFun,
+    ) where
+
+import Employee
+import Data.Monoid
+
+-- Exercise 1
+
+-- Adds an Employee to the GuestList (updating the cached Fun score appropriately)
+--
+-- Of course, in general this is impossible:
+-- the updated fun score should depend on whether the Employee
+-- being added is already in the list, or if any of their direct subordinates
+-- are in the list, and so on. For our purposes, though, you
+-- may assume that none of these special cases will hold: that is,
+-- glCons should simply add the new Employee and add their fun
+-- score without doing any kind of checks.
+glCons :: Employee -> GuestList -> GuestList
+glCons e (GL gl fun) = GL (gl ++ [e]) (fun + empFun e)
+
+-- A Monoid instance for GuestList
+
+instance Monoid GuestList where
+    mempty = GL [] 0
+    mappend (GL gl1 fun1) (GL gl2 fun2) = GL (gl1 ++ gl2) (fun1 + fun2)
+
+-- Takes two GuestLists and returns whichever one of them
+-- is more fun, i.e. has the higher fun score. (If the scores are equal it
+-- does not matter which is returned.)
+moreFun :: GuestList -> GuestList -> GuestList
+moreFun gl1@(GL _ fun1) gl2@(GL _ fun2) = if fun1 > fun2 then gl1 else gl2
+
+
+-- Exercise 2
+
+treeFold :: (a -> b -> b) -> b -> Tree a -> b
+treeFold func b tree = (func (rootLabel tree) b) 
---
+++ test/Ch8Spec.hs
@@ -0,0 +1,31 @@
+import Test.Hspec
+import Test.QuickCheck
+import System.IO
+import Ch8
+import Employee
+
+
+main = hspec $ do
+
+    describe "Ch8" $ do 
+
+        describe "glCons" $ do
+            let e = Emp "Stan" 9
+            let gl = GL [Emp "Joe" 5] 5
+
+            it "adds an Employee to the GuestList (updating the cached Fun score appropriately)" $ do
+                glCons e gl `shouldBe` GL [Emp "Joe" 5, Emp "Stan" 9] 14
+    
+        describe "Monoid instance for GuestList" $ do
+            let gl1 = GL [Emp "Stan" 9] 9
+            let gl2 = GL [Emp "Joe" 5] 5
+
+            it "can do monoidic things" $ do
+                gl1 `mappend` gl2 `shouldBe` GL [Emp "Stan" 9, Emp "Joe" 5] 14
+
+        describe "moreFun" $ do
+            let gl1 = GL [Emp "Stan" 9] 9
+            let gl2 = GL [Emp "Joe" 5] 5
+
+            it "returns a funer GuestList" $ do
+                moreFun gl1 gl2 `shouldBe` gl1
